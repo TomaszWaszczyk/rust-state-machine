f03052911e19888529e73d0972ab62da6bc5d7e2
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-11-14 18:12:10 -0400

    solution: add set and get balances

--- src/balances.rs
+++ src/balances.rs
@@ -16,12 +16,12 @@ impl Pallet {
 
 	/// Set the balance of an account `who` to some `amount`.
 	pub fn set_balance(&mut self, who: &'static str, amount: u128) {
-		/* Insert `amount` into the BTreeMap under `who`. */
+		self.balances.insert(who, amount);
 	}
 
 	/// Get the balance of an account `who`.
 	/// If the account has no stored balance, we return zero.
 	pub fn balance(&self, who: &'static str) -> u128 {
-		/* Return the balance of `who`, returning zero if `None`. */
+		*self.balances.get(&who).unwrap_or(&0)
 	}
 }
