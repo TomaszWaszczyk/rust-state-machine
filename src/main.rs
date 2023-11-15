8c33ce21f3d50a444d1cdc996d8c3df793337870
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-11-14 21:36:37 -0400

    template: tightly couple balances to system

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ impl system::Config for Runtime {
 }
 
 impl balances::Config for Runtime {
+	/* TODO: After inheriting from the `system::Config` trait, you won't need `AccountId` here. */
 	type AccountId = &'static str;
 	type Balance = u128;
 }
