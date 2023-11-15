9754ef630fbf82a565195a2dccba07434ec4c914
Shawn Tabrizi <shawntabrizi@gmail.com>
2023-11-14 20:55:17 -0400

    template: make system module configurable

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,12 @@ type Balance = u128;
 type BlockNumber = u32;
 type Nonce = u32;
 
+/*
+	TODO:
+	Implement the `system::Config` trait you created on your `Runtime`.
+	Use `Self` to satisfy the generic parameter required for `system::Pallet`.
+*/
+
 // This is our main Runtime.
 // It accumulates all of the different pallets we want to use.
 #[derive(Debug)]
